a7822cbd342d8c6b7e6a8a08b10faa2e316a0a33
douabaghdadi <azizbelhadj51@gmail.com>
2025-09-27 00:04:53 +0100

    Recherche catégorie front

--- app/Http/Controllers/FrontOfficeController.php
+++ app/Http/Controllers/FrontOfficeController.php
@@ -15,9 +15,25 @@ class FrontOfficeController extends Controller
         return view('FrontOffice.Accueil', compact('categories', 'blogs'));
     }
 
-    public function categories()
+    public function categories(Request $request)
     {
-        $categories = Category::all();
+        $query = Category::query();
+
+        if ($request->filled('search')) {
+            $search = $request->search;
+            $searchType = $request->get('search_type', 'name');
+            
+            if ($searchType == 'name') {
+                $query->where('name', 'LIKE', "%{$search}%");
+            } else {
+                $query->where('description', 'LIKE', "%{$search}%");
+            }
+        }
+
+        $sortOrder = $request->get('sort', 'asc');
+        $query->orderBy('name', $sortOrder);
+
+        $categories = $query->get();
         return view('FrontOffice.Categories.CategoriesPage', compact('categories'));
     }
 }
