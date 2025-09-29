9ce6da51bed7de7644a463ed75cd9230503c915d
YOSRcharek <yosryassoura31@gmail.com>
2025-09-29 06:43:14 +0100

    notif

--- app/Http/Controllers/AccueilController.php
+++ app/Http/Controllers/AccueilController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Blog;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class AccueilController extends Controller
 {
         public function index()
@@ -12,4 +12,6 @@ class AccueilController extends Controller
         $blogs = Blog::latest()->take(3)->get();
         return view('FrontOffice.Accueil', compact('blogs'));
     }
+
+   
 }
